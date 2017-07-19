cbc9d54eb63486ec265d9c2611efe5e68e5f5dbe
Byron Polley <byronpolley@live.com>
2017-07-19 12:30:35 +0200

    brand styling

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var baseNodes = [{
+var nodes = [{
     id: "organisational",
     group: 0,
     label: "GCRO Organisational",
@@ -420,7 +420,7 @@ var baseNodes = [{
   },
 ]
 
-var baseLinks = [{
+var links = [{
     target: "organisational",
     source: "thematic1",
     strength: 0.1
@@ -1122,18 +1122,17 @@ var baseLinks = [{
   },
 ]
 
-var nodes = [...baseNodes]
-var links = [...baseLinks]
-
 function getNeighbors(node) {
-  return baseLinks.reduce(function(neighbors, link) {
-    if (link.target.id === node.id) {
-      neighbors.push(link.source.id)
-    } else if (link.source.id === node.id) {
-      neighbors.push(link.target.id)
-    }
-    return neighbors
-  }, [node.id])
+  return links.reduce(function (neighbors, link) {
+      if (link.target.id === node.id) {
+        neighbors.push(link.source.id)
+      } else if (link.source.id === node.id) {
+        neighbors.push(link.target.id)
+      }
+      return neighbors
+    },
+    [node.id]
+  )
 }
 
 function isNeighborLink(node, link) {
@@ -1146,17 +1145,16 @@ function getNodeColor(node, neighbors) {
     return node.level === 1 ? 'white' : 'red'
   }
 
-  return node.level === 1 ? 'blue' : 'pink'
-
+  return node.level === 1 ? '#832a2a' : '#9A9C99'
 }
 
 
 function getLinkColor(node, link) {
-  return isNeighborLink(node, link) ? 'red' : '#E5E5E5'
+  return isNeighborLink(node, link) ? 'white' : 'red'
 }
 
 function getTextColor(node, neighbors) {
-  return Array.isArray(neighbors) && neighbors.indexOf(node.id) > -1 ? 'black' : 'black'
+  return Array.isArray(neighbors) && neighbors.indexOf(node.id) > -1 ? 'red' : 'white'
 }
 
 var width = window.innerWidth
@@ -1165,28 +1163,11 @@ var height = window.innerHeight
 var svg = d3.select('svg')
 svg.attr('width', width).attr('height', height)
 
-var linkElements,
-  nodeElements,
-  textElements
-
-// we use svg groups to logically group the elements together
-var linkGroup = svg.append('g').attr('class', 'links')
-var nodeGroup = svg.append('g').attr('class', 'nodes')
-var textGroup = svg.append('g').attr('class', 'texts')
-
-// we use this reference to select/deselect
-// after clicking the same element twice
-var selectedId
-
 // simulation setup with all forces
 var linkForce = d3
   .forceLink()
-  .id(function(link) {
-    return link.id 
-  })
-  .strength(function(link) {
-    return link.strength 
-  })
+  .id(function (link) { return link.id })
+  .strength(function (link) { return link.strength })
 
 var simulation = d3
   .forceSimulation()
@@ -1194,14 +1175,14 @@ var simulation = d3
   .force('charge', d3.forceManyBody().strength(-120))
   .force('center', d3.forceCenter(width / 2, height / 2))
 
-var dragDrop = d3.drag().on('start', function(node) {
+var dragDrop = d3.drag().on('start', function (node) {
   node.fx = node.x
   node.fy = node.y
-}).on('drag', function(node) {
+}).on('drag', function (node) {
   simulation.alphaTarget(0.7).restart()
   node.fx = d3.event.x
   node.fy = d3.event.y
-}).on('end', function(node) {
+}).on('end', function (node) {
   if (!d3.event.active) {
     simulation.alphaTarget(0)
   }
@@ -1209,176 +1190,56 @@ var dragDrop = d3.drag().on('start', function(node) {
   node.fy = null
 })
 
-// select node is called on every click
-// we either update the data according to the selection
-// or reset the data if the same node is clicked twice
 function selectNode(selectedNode) {
-
-  console.log(selectedNode);
-  if (selectedId === selectedNode.id) {
-    selectedId = undefined
-    resetData()
-    updateSimulation()
-  } else {
-    selectedId = selectedNode.id
-    updateData(selectedNode)
-    updateSimulation()
-  }
-
   var neighbors = getNeighbors(selectedNode)
 
   // we modify the styles to highlight selected nodes
-  nodeElements.attr('fill', function(node) {
-    return getNodeColor(node, neighbors) 
-  })
-  textElements.attr('fill', function(node) {
-    return getTextColor(node, neighbors)
-  })
-  linkElements.attr('stroke', function(link) {
-    return getLinkColor(selectedNode, link)
-  })
+  nodeElements.attr('fill', function (node) { return getNodeColor(node, neighbors) })
+  textElements.attr('fill', function (node) { return getTextColor(node, neighbors) })
+  linkElements.attr('stroke', function (link) { return getLinkColor(selectedNode, link) })
 }
 
-// this helper simple adds all nodes and links
-// that are missing, to recreate the initial state
-function resetData() {
-  var nodeIds = nodes.map(function(node) {
-    return node.id
-  })
+var linkElements = svg.append("g")
+  .attr("class", "links")
+  .selectAll("line")
+  .data(links)
+  .enter().append("line")
+    .attr("stroke-width", 1)
+	  .attr("stroke", "#9A9C99")
 
-  baseNodes.forEach(function(node) {
-    if (nodeIds.indexOf(node.id) === -1) {
-      nodes.push(node)
-    }
-  })
-
-  links = baseLinks
-}
-
-// diffing and mutating the data
-function updateData(selectedNode) {
-  var neighbors = getNeighbors(selectedNode)
-  var newNodes = baseNodes.filter(function(node) {
-    return neighbors.indexOf(node.id) > -1 || node.level === 1
-  })
-
-  var diff = {
-    removed: nodes.filter(function(node) {
-      return newNodes.indexOf(node) === -1
-    }),
-    added: newNodes.filter(function(node) {
-      return nodes.indexOf(node) === -1
-    })
-  }
-
-  diff.removed.forEach(function(node) {
-    nodes.splice(nodes.indexOf(node), 1)
-  })
-  diff.added.forEach(function(node) {
-    nodes.push(node)
-  })
-
-  links = baseLinks.filter(function(link) {
-    return link.target.id === selectedNode.id || link.source.id === selectedNode.id
-  })
-}
-
-function updateGraph() {
-  // links
-  linkElements = linkGroup.selectAll('line')
-    .data(links, function(link) {
-      return link.target.id + link.source.id
-    })
-
-  linkElements.exit().remove()
-
-  var linkEnter = linkElements
-    .enter().append('line')
-    .attr('stroke-width', 1)
-    .attr('stroke', 'rgba(50, 50, 50, 0.2)')
-
-  linkElements = linkEnter.merge(linkElements)
-
-  // nodes
-  nodeElements = nodeGroup.selectAll('circle')
-    .data(nodes, function(node) {
-      return node.id
-    })
-
-  nodeElements.exit().remove()
-
-  var nodeEnter = nodeElements
-    .enter()
-    .append('circle')
-    .attr('r', 20)
-    .attr('fill', function(node) {
-      return node.level === 1 ? 'red' : 'white'
-    })
+var nodeElements = svg.append("g")
+  .attr("class", "nodes")
+  .selectAll("circle")
+  .data(nodes)
+  .enter().append("circle")
+    .attr("r", 20)
+    .attr("fill", getNodeColor)
     .call(dragDrop)
-    // we link the selectNode method here
-    // to update the graph on every click
     .on('click', selectNode)
 
-  nodeElements = nodeEnter.merge(nodeElements)
+var textElements = svg.append("g")
+  .attr("class", "texts")
+  .selectAll("text")
+  .data(nodes)
+  .enter().append("text")
+    .text(function (node) { return  node.label })
+	  .attr("font-size", 15)
+	  .attr("dx", 15)
+    .attr("dy", 4)
+    .style('fill', 'white')
 
-  // texts
-  textElements = textGroup.selectAll('text')
-    .data(nodes, function(node) {
-      return node.id
-    })
+simulation.nodes(nodes).on('tick', () => {
+  nodeElements
+    .attr('cx', function (node) { return node.x })
+    .attr('cy', function (node) { return node.y })
+  textElements
+    .attr('x', function (node) { return node.x })
+    .attr('y', function (node) { return node.y })
+  linkElements
+    .attr('x1', function (link) { return link.source.x })
+    .attr('y1', function (link) { return link.source.y })
+    .attr('x2', function (link) { return link.target.x })
+    .attr('y2', function (link) { return link.target.y })
+})
 
-  textElements.exit().remove()
-
-  var textEnter = textElements
-    .enter()
-    .append('text')
-    .text(function(node) {
-      return node.label
-    })
-    .attr('font-size', 15)
-    .attr('dx', 15)
-    .attr('dy', 4)
-
-  textElements = textEnter.merge(textElements)
-}
-
-function updateSimulation() {
-  updateGraph()
-
-  simulation.nodes(nodes).on('tick', () => {
-    nodeElements
-      .attr('cx', function(node) {
-        return node.x
-      })
-      .attr('cy', function(node) {
-        return node.y
-      })
-    textElements
-      .attr('x', function(node) {
-        return node.x
-      })
-      .attr('y', function(node) {
-        return node.y
-      })
-    linkElements
-      .attr('x1', function(link) {
-        return link.source.x
-      })
-      .attr('y1', function(link) {
-        return link.source.y
-      })
-      .attr('x2', function(link) {
-        return link.target.x
-      })
-      .attr('y2', function(link) {
-        return link.target.y
-      })
-  })
-
-  simulation.force('link').links(links)
-  simulation.alphaTarget(0.7).restart()
-}
-
-// last but not least, we call updateSimulation
-// to trigger the initial render
-updateSimulation()
+simulation.force("link").links(links)
